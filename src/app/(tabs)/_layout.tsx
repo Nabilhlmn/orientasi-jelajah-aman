97901596c0d3dc83e041a34fd29a64867c389799
Muhammad Nabil Hilman <hilman.nabil2486@gmail.com>
2026-09-22 09:39:23 +0700

    feat: buat tab navigation Beranda, Riwayat, Pengaturan

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,18 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
+import { Tabs } from "expo-router";
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+return (
+<Tabs screenOptions={{ headerShown: true }}>
+<Tabs.Screen name="index" options={{ title: "Beranda" }} />
+<Tabs.Screen name="riwayat" options={{ title: "Riwayat" }} />
+<Tabs.Screen name="pengaturan" options={{ title: "Pengaturan" }} />
+</Tabs>
+);
 }
